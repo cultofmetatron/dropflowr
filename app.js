faacd18e8a69c54dbeb24a7862e745b98c52383a
cultofmetatron <cultofmetatron@aumlogic.com>
2013-04-27 20:48:02 -0700

    [wip] going to redo the auth using passport instead

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 var configs = require ('./configuration.js');
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -14,27 +13,41 @@ var ROOT = __dirname + '';
 
 var app = express();
 var everyauth = require('everyauth');
-
+everyauth.debug = true;
 //mongodb database
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/disco1');
 
 //declare our models
 var DropboxUser = require('./models/user.js')(mongoose);
-console.log(DropboxUser);
 
 //everyauth configuration
+everyauth.everymodule.userPkey('uid');
+everyauth.everymodule.findUserById( function (userId, callback) {
+//  User.findById(userId, callback);
+  // callback has the signature, function (err, user) {...}
+//}
+  console.log('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%findUserById in app.js');
+  DropboxUser.findUser(userId, function(err, user) {
+    console.log('got the user &&&&&&&&&&&&&&& ', user);
+    callback(err, user);
+  });
+});
+
 everyauth.dropbox
   .consumerKey(configs.db_consumerKey)
   .consumerSecret(configs.db_consumerSecret)
   .findOrCreateUser(function (sess, accessToken, accessSecret, user) {
+    console.log('session', sess);
     //login to database logic goes here!!
     var deferred = Q.defer();
     DropboxUser.findOrCreateUser(user, function(err, user) {
       if (user) {
         //user was either found or created and we can create the session
-        deferred.resolve(user.id);
+        console.log('######## ', user, user.uid);
+        deferred.resolve(user);
       } else {
+        console.log('##############there was an error!!! ###################');
         return deferred.resolve(0);
       }
     });
@@ -51,12 +64,13 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-app.use(everyauth.middleware());
-app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+//app.use(express.session({ secret: 'htuayreve', store: MemStore({reapInterval: 60000 * 10})}));
+app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(everyauth.middleware(app));
+app.use(app.router);
 
 // development only
 if ('development' == app.get('env')) {
@@ -64,14 +78,14 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', function(req, res) {
-  console.log(req.user);
+  console.log('req.session', req.session);
+  debugger
   res.render('index', {
     title: "yay",
-    user : req.user
+    user : everyauth.user
   });
 
 });
-app.get('/users', user.list);
 app.get('/login', function(req, res) {
   option = {};
   if (req.user) { console.log(req.user); }
--- models/user.js
+++ models/user.js
@@ -7,7 +7,6 @@ module.exports = function(mongoose) {
     id            :  {type: Number, unique: true },
     email         :  {type: String},
     display_name  :  {type: String},
-    referal_link  :  {type: String},
     country       :  {type: String},
     quota_info: {
       shared : Number,
@@ -19,8 +18,7 @@ module.exports = function(mongoose) {
 
   //return a user if he exists in our database already
   DropboxUserSchema.statics.findUser = function(uid, callback) {
-    console.log('it gets here');
-    DropboxUser.findOne({
+    return DropboxUser.findOne({
       uid: uid
     }, callback );
   };
@@ -31,7 +29,7 @@ module.exports = function(mongoose) {
    */
   DropboxUserSchema.statics.findOrCreateUser = function(user, callback) {
     console.log('in findOrCreateUser');
-    DropboxUser.findUser(user.uid, function(err, usr) {
+    return DropboxUser.findUser(user.uid, function(err, usr) {
       if (!usr) {
         console.log('user was not found, creating one now');
         //user not found so we shall create one
@@ -53,9 +51,8 @@ module.exports = function(mongoose) {
         });
       } else {
         //user exists so we update the validated user's info
-        console.log('&&&&&&&&&&& usr', usr);
         DropboxUser.update({uid: usr.uid}, {
-          referal_link   : user.referal_link,
+          //referal_link   : user.referal_link,
           display_name   : user.display_name,
           email          : user.email,
           country        : user.country,
@@ -64,8 +61,8 @@ module.exports = function(mongoose) {
           if (err) {
             console.log('error: ' + err );
           }
+          console.log('updated user: ', usr);
           //pass info back up the stack
-          console.log('does this run?');
           return callback(null, usr);
         });
               }
